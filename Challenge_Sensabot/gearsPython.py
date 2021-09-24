d5b6ece789ef3bd8991161fba61c500a5281f76f
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-09-24 09:47:25 -0400

    Add files via upload

---
+++ Challenge_Sensabot/gearsPython.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Import the necessary libraries
+import time
+import math
+from ev3dev2.motor import *
+from ev3dev2.sound import Sound
+from ev3dev2.sensor import *
+from ev3dev2.sensor.lego import *
+from ev3dev2.sensor.virtual import *
+
+# Create the sensors and motors objects
+motorA = LargeMotor(OUTPUT_A)
+motorB = LargeMotor(OUTPUT_B)
+left_motor = motorA
+right_motor = motorB
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
+
+spkr = Sound()
+radio = Radio()
+
+
+
+# Here is where your code starts
+
