3eee961f397971e54607897b233de507481c9b4f
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-09-23 23:59:27 -0400

    Add files via upload

---
+++ ArmControl/gearsPython.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Import the necessary libraries
+import time
+import math
+from ev3dev2.motor import *
+from ev3dev2.sound import Sound
+from ev3dev2.sensor import *
+from ev3dev2.sensor.lego import *
+from ev3dev2.sensor.virtual import *
+
+# Create the sensors and motors objects
+motorA = LargeMotor(OUTPUT_A)
+motorB = LargeMotor(OUTPUT_B)
+left_motor = motorA
+right_motor = motorB
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
+
+spkr = Sound()
+radio = Radio()
+
+
+motorC = LargeMotor(OUTPUT_C) # Arm
+
+# Here is where your code starts
+
